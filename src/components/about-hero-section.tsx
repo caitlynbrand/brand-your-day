2e6fb85b4554c59f20932e4fc55d9ca425db1d77
dylangilson <dylan.gilson@outlook.com>
2026-03-30 23:07:53 -0700

    updated margin for about page

--- src/components/about-hero-section.tsx
+++ src/components/about-hero-section.tsx
@@ -40,7 +40,7 @@ export default function HeroSection() {
     <>
       {/* <HeroHeader /> */}
 
-      <main className="overflow-hidden mt-20 md:-mt-30">
+      <main className="overflow-hidden -mt-20 md:-mt-30">
         {/* Background blobs */}
         <div
           aria-hidden
